7ae7fa191dbafc61b8843e31a10b3a44f5981f15
asshikrani <asshikrani66@gmail.com>
2024-10-29 00:09:57 +0500

    QR_code.py

--- QR_code.py
+++ QR_code.py
@@ -3,15 +3,4 @@ import qrcode as qr
 
 img = qr.make("paste link here:")
 img.save("qwerty123.png")
-
-# qrc = qr.QRCode(version=1,
-#                 error_correction=qr.constant.ERROR_CORRECT_H,
-#                 box_size=10,border=4,)
-
-# qrc.add_data("https://youtu.be/TH9NCT-q1Fs?si=aDK-HGSH4DpI3SqM")
-
-# qrc.make(fit=True)
-
-# img = qrc.make_image(fill_color='brown', back_color='cyan')
-
-# img.save("video.png  ")
+
