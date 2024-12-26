747d9c6d2a64d34cb9769f28b6687bba3b7fa36c
Ahmad Siddique <asshikrani66@gmail.com>
2024-12-26 10:57:20 +0500

    Update QR_code.py
    I add comments in this file for better understanding

--- QR_code.py
+++ QR_code.py
@@ -1,6 +1,15 @@
+""" 
+Before running this code make sure that qrcode module is installed in your system 
+if not then run this command in you terminal or command-prompt;
+(i)_For windows:
+  (pip install qrcode)
+(ii)_For macos:
+  pip3 install qrcode
+(iii)_For linux:
+  pip3 install qrcode
+"""
 import qrcode as qr
-# from PIL import image
 
 img = qr.make("paste link here:")
 img.save("qwerty123.png")
-
+
