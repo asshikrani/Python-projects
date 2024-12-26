28a335f2163017043fc51cbd90cf1967f94d273b
Ahmad Siddique <asshikrani66@gmail.com>
2024-12-26 11:17:55 +0500

    Update w_Automation_by_pyautogui.py

--- w_Automation_by_pyautogui.py
+++ w_Automation_by_pyautogui.py
@@ -1,3 +1,25 @@
+# This is a very sensitive program.
+""" 
+Before running this code make sure that qrcode module is installed in your system 
+if not then run this command in you terminal or command-prompt;
+(i)_For windows:
+  pip install tkinter
+  pip install pyjokes
+(ii)_For macos:
+  pip3 install tkinter
+  pip3 install py jokes
+(iii)_For linux:
+  pip3 install tkinter
+  pip install pyjokes
+"""
+
+"""
+You have to set cordinnates(position) accourding to your system.
+You can check cordinates by simply using this code:
+while True:
+    position = pg.position()
+    print(position)
+"""
 import pyautogui as ag
 import time
 import pyjokes
@@ -26,7 +48,6 @@ def execute():
 if __name__ == '__main__':
     execute()
 
-# a = pyjokes.get_jokes()
-# print(a)
+
 
 
